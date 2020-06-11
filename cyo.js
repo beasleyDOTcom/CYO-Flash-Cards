3e5c6786e8f4795342baf80bba4cfaeb2effd1b3
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2020-06-11 00:04:21 -0700

    Update cyo.js
    needed to go one step further to find the find true by using the random number generated to find the key using key() then get the value of the key from get(key) to evaluate to the user entry to true;

--- cyo.js
+++ cyo.js
@@ -4,20 +4,27 @@ function createCard(){
        localStorage.setItem(promptKey,answerValue);
        location.reload();
     };
+var conk = document.getElementById("eraseMe");
+if(conk){
+  conk.addEventListener("click", eraseMe);}
     function eraseMe(){
         localStorage.clear();
     };
         var gameKey=0;
+  var bonk = document.getElementById("startButton");
+if(bonk){bonk.addEventListener("click", startGame);}
     function startGame(){
          gameKey=Math.floor(Math.random()*localStorage.length);
         document.getElementById("prompt").innerHTML=localStorage.key(gameKey);
-      
+      var crank = document.getElementById("resultButton");
+      if(crank){crank.addEventListener("click", submitAnswer);}
      function submitAnswer(){
         let userAnswer=document.getElementById("userAnswer").value;
         let keyName=localStorage.key(gameKey);
+        let valuePair = localStorage.get(keyName);
        console.log(keyName);
        console.log(userAnswer);
-        if(userAnswer==keyName){
+        if(userAnswer==valuePair){
             document.getElementById("result").innerHTML= "YOU DID IT!!!";
           }
         else {
@@ -25,4 +32,6 @@ function createCard(){
              }
           }
        }
-
+console.log("Poopy")
+var monster = document.getElementById("createButton");
+if(monster){monster.addEventListener("click", createCard);}

d05531b45279cf8b0534ba362b2ba85762a38a60
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2020-06-10 20:48:42 -0700

    Update cyo.js
    first function.

--- cyo.js
+++ cyo.js
@@ -1,1 +1,9 @@
-javascript
+//this function takes two inputs and forms a key(prompt) and value(answer) from their relative inputs. 
+
+//this function should also send that key/value to local storage.
+function createCard(){  
+    let promptKey=document.getElementById("promptKey").value;
+    let answerValue=document.getElementById("answerValue").value;
+       localStorage.setItem(promptKey,answerValue);
+       location.reload();
+    };

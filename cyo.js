2239e3d6b8587e2c93cc9080e49e8e69a2419e22
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2020-06-10 23:01:46 -0700

    Update cyo.js
    added reload to reset function.

--- cyo.js
+++ cyo.js
@@ -1,6 +1,3 @@
-//this function takes two inputs and forms a key(prompt) and value(answer) from their relative inputs. 
-
-//this function should also send that key/value to local storage.
 function createCard(){  
     let promptKey=document.getElementById("promptKey").value;
     let answerValue=document.getElementById("answerValue").value;
@@ -18,12 +15,13 @@ function createCard(){
      function submitAnswer(){
         let userAnswer=document.getElementById("userAnswer").value;
         let keyName=localStorage.key(gameKey);
+       console.log(keyName);
         if(userAnswer==keyName){
             document.getElementById("result").innerHTML= "YOU DID IT!!!";
         }
         else {
             document.getElementById("result").innerHTML = "You did... something?"
         };
-         console.log(keyName);
-         console.log(userAnswer);
+       location.reload();
     };
+

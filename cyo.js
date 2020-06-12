e744fedae47ff932f08883133c67c2aecc4c8c2c
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2020-06-12 01:37:56 -0700

    added some helpful comments to describe my functions.

--- cyo.js
+++ cyo.js
@@ -1,3 +1,5 @@
+//I start a trend of cueing up some event listeners that trigger their respective functions.
+//createCard takes user input and stores it in local storage. 
 var monster = document.getElementById("createButton");
 if(monster){monster.addEventListener("click", createCard);}
 function createCard(){  
@@ -17,13 +19,15 @@ var gameKey=0;
 var bonk = document.getElementById("startButton");
 if(bonk)
     {bonk.addEventListener("click", startGame);}
-    
+//This shows instructions to start game that get overwritten after the game starts.    
     document.getElementById("result").innerHTML = 'Press "New Card" to begin.';
+// generates a random number that selects and presents a key(front size of flash card) and presents it to user.
 function startGame(){
     gameKey=Math.floor(Math.random()*localStorage.length);
     document.getElementById("result").innerHTML=localStorage.key(gameKey);
     var crank = document.getElementById("resultButton");
     if(crank){crank.addEventListener("click", submitAnswer);}
+//This uses the random number to get the key() then uses the key to get() the backside of flashcard then compares it to user input.    
         function submitAnswer(){
             let userAnswer=document.getElementById("userAnswer").value;
             let keyName=localStorage.key(gameKey);
